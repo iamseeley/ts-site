815d727af80f4bdb3d5e4d50e580225e72ce0920
iamseeley <seeleypersonal@gmail.com>
2023-11-04 18:47:54 -0400

    adding pdf viewer

---
+++ src/components/ui/document.tsx
@@ -0,0 +1,13 @@
+// import { Document } from "@react-pdf/renderer";
+
+
+const PDFViewer = () => {
+//  const pdfURL = 'path_to_pdf_file.pdf';
+return (
+ <div>
+ {/* <Document file={pdfURL}>
+ </Document> */}
+ </div>
+ );
+};
+export default PDFViewer;
