ca5df1c5ffa2a0148507d7ed5d3ea3f17bf5bb02
iamseeley <seeleypersonal@gmail.com>
2023-11-05 10:04:54 -0500

    changing project description

--- src/components/ui/projectlist.tsx
+++ src/components/ui/projectlist.tsx
@@ -8,7 +8,7 @@ function ProjectList() {
                 <h3>Projects</h3>
                 <ul>
                 <Project link="https://github.com/iamseeley/go-forth" title="go-forth" description="A template for building static sites with Go" />
-                <Project link="https://github.com/iamseeley/wando-ui" title="wando-ui" description="Component collection for web projects - built with TypeScript, styled with Tailwind CSS" />
+                <Project link="https://github.com/iamseeley/wando-ui" title="wando-ui" description="Component collection - built with TypeScript, styled with Tailwind CSS" />
                 </ul>
            </section> 
         </>
