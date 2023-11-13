9e5e061bfad6cf2e29a44a493aaa7bef2620c17d
iamseeley <seeleypersonal@gmail.com>
2023-11-13 12:28:01 -0500

    adding projects

--- src/components/ui/projectlist.tsx
+++ src/components/ui/projectlist.tsx
@@ -9,6 +9,8 @@ function ProjectList() {
                 <ul>
                 <ProjectItem link="https://github.com/iamseeley/go-forth" title="go-forth" description="A template for building static sites with Go" />
                 <ProjectItem link="https://github.com/iamseeley/wando-ui" title="wando-ui" description="Component collection - built with TypeScript, styled with Tailwind CSS" />
+                <ProjectItem link="https://github.com/iamseeley/ts-site" title="ts-site" description="Current personal website - powered by Vite and React" />
+                <ProjectItem link="https://github.com/iamseeley/ts-wiki" title="ts-wiki" description="Archived personal wiki" />
                 </ul>
            </section> 
         </>
