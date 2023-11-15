eb302d8355ca17cae6aadd61f498ec9b9633693b
iamseeley <seeleypersonal@gmail.com>
2023-11-15 09:03:40 -0500

    fixing metadata

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -3,11 +3,16 @@ import ProjectList from "../components/ui/projectlist"
 import Header from "../components/header";
 import Footer from "../components/footer";
 import { MotionLayout } from "../components/motionlayout";
+import { Helmet } from "react-helmet-async";
 
 function Home() {
     
     return (
         <>
+            <Helmet>
+                <title>Thomas Seeley</title>
+                <meta name='description' content=""/>
+            </Helmet>
             <MotionLayout>
                 <Header />
                 <ProjectList />
